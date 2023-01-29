33ee512f2e65167c5946bbd11f16cd9bd6a05108
Armando Salazar <armandosalazar351@gmail.com>
2023-01-28 23:41:13 -0600

    Add fundamental concepts of variables and const

--- src/fundamental_concepts.rs
+++ src/fundamental_concepts.rs
@@ -1,8 +1,23 @@
-/* Variables */
-fn variables() {
+/* Variables & Constantes */
+fn variables_and_const() {
+    /* Variables */
     let x = 0;
     let y: u8 = 1;
-    // Las variables en rust son inmutables por defecto
+    // Las variables en rust son inmutables por defecto.
     let mut z = 0;
     z = 1;
+    println!("{} {} {}", x, y, z);
+
+    /* Scope and Shadowing */
+    // El shadowing puede ser util para cambiar el tipo de dato
+    // o para cambiar el valor a una variable inmutable.
+    let x = "Text";
+
+    /* Constante */
+    // Las constantes se tienen que asignar al declararse
+    // también se les asigna su respectivo tipo de dato
+    // a diferencia de las variables.
+    // Las constantes pueden estar en cualquier scope,
+    // como variables globales.
+    const IQ: u8 = 180;
 }
