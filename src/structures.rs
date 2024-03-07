608b1fa9f3e8f369197e94695137487d225ea3da
Armando Salazar <armandosalazar351@gmail.com>
2024-03-07 02:35:20 -0600

    eefactor: remove unused code and add LinkedList implementation

--- src/structures.rs
+++ src/structures.rs
@@ -1,4 +1,32 @@
-fn private_function() {}
-pub fn function(name: &str) {
-    println!("Hi {}!", name)
+// @todo - Permitir variables sin usar en todo el archivo
+#![allow(unused_variables)]
+#![allow(unused_imports)]
+#![allow(dead_code)]
+#[derive(Debug)]
+struct Node {
+    value: u8,
+    next: Option<Box<Node>>
+}
+#[derive(Debug)]
+pub struct LinkedList {
+    head: Option<Box<Node>>
+}
+
+impl LinkedList {
+    pub fn new() -> LinkedList {
+        LinkedList { head: None }
+    }
+
+    pub fn push(&mut self, value: u8) {
+        let node: Node = Node {
+            value: value,
+            next: None
+        };
+        if self.head.is_none() {
+            self.head = Some(Box::new(node));
+        } else {
+            let current = &mut self.head;
+            print!("{:?}", *current);
+        }
+    }
 }
